0da8154540c265835d68ad2448fa56a804f43718
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-10-23 11:22:54 +0900

    b

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ tree = app_commands.CommandTree(client)
 # configをロード
 config = json.load(open("config.json", "r"))
 
-# テラーリストと対応音源をロード
+# テラーリストをロード
 terror_file = json.load(open("./audio/terror.json", "r"))
 
 @client.event
@@ -24,22 +24,33 @@ async def on_ready():
     print("i'm ready!")
     print("-" * 16)
 
-# スラッシュコマンド登録
+# スラッシュコマンド実装
+
+# ゲーム中かどうかの判定フラグ
+
+isStart = False
 
 @tree.command(name="play", 
               description="イントロドンを開始")
 async def playgame(interaction: discord.Interaction):
     user_voice_channel_name =  interaction.user.voice.channel.name
     user_voice_channel_id =    interaction.user.voice.channel.id # 音声再生チャンネル
-    user_answer_channel_name = interaction.channel.name
-    user_answer_channel_id =   interaction.channel.id # 回答受付チャンネル
+    # user_answer_channel_name = interaction.channel.name
+    # user_answer_channel_id =   interaction.channel.id # 回答受付チャンネル
     await interaction.response.send_message(f"""
-以下のボイスチャンネルに参加し、このチャンネルを回答用チャンネルとしてマークします！
+以下のボイスチャンネルに参加してゲームを開始します！
 ボイスチャンネル 名前: `{user_voice_channel_name}` | ボイスチャンネル ID: `{user_voice_channel_id}`
-回答用チャンネル 名前: `{user_answer_channel_name}` | 回答用チャンネル ID: `{user_answer_channel_id}`
-
-マーク解除するには `終了` と発言することでマーク解除できます！
 """)
-    
+    global isStart
+    isStart = True
+
+@tree.command(name="answer",
+            description="イントロドンに回答")
+async def answergame(interaction: discord.Interaction):
+    if isStart == True:
+        await interaction.response.send_message("a")
+    else:
+        await interaction.response.send_message("b")
+
 
 client.run(config["token"])
